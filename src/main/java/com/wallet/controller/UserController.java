e3b926cce8154b35822bdb195d2d1cb24760acc9
srobertot <60357002+srobertot@users.noreply.github.com>
2020-06-24 11:03:11 -0300

    Foi criado UserController e os seus testes

---
+++ src/main/java/com/wallet/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.wallet.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wallet.dto.UserDTO;
+import com.wallet.entity.User;
+import com.wallet.response.Response;
+import com.wallet.service.UserService;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO userDTO, BindingResult result){
+		
+		Response<UserDTO> response = new Response<UserDTO>();
+		User user = userService.save(convertDtoToEntity(userDTO));
+		response.setData(convertEntityToDto(user));
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	private User convertDtoToEntity(UserDTO userDTO) {
+		User u = new User();
+		u.setEmail(userDTO.getEmail());
+		u.setName(userDTO.getName());
+		u.setPassword(userDTO.getPassword());
+		
+		return u;
+	}
+	
+	private UserDTO convertEntityToDto(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setEmail(user.getEmail());
+		userDTO.setName(user.getName());
+		userDTO.setPassword(user.getPassword());
+		
+		return userDTO;
+	}
+	
+}
